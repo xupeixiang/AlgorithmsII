b5fc9c9c70ca20f6ea0dcc450f2fcc15ca3542d8
Peixiang Xu <peixiangxu@gmail.com>
2013-04-25 12:05:07 +0800

    License declaration added

--- AlgorithmsII/WeekOne/Outcast.java
+++ AlgorithmsII/WeekOne/Outcast.java
@@ -1,3 +1,13 @@
+/************************************************************
+ * 
+ * Copyright (c) 2013, Peixiang Xu(peixiangxu@gmail.com)
+ * 
+ * This program is free software: you can redistribute it 
+ * and/or modify it under the terms of the GNU General Public 
+ * License as published by the Free Software Foundation.
+ * 
+ ************************************************************/
+
 public class Outcast {
 	
 	private WordNet wordNet = null;
--- AlgorithmsII/WeekOne/SAP.java
+++ AlgorithmsII/WeekOne/SAP.java
@@ -1,3 +1,12 @@
+/************************************************************
+ * 
+ * Copyright (c) 2013, Peixiang Xu(peixiangxu@gmail.com)
+ * 
+ * This program is free software: you can redistribute it 
+ * and/or modify it under the terms of the GNU General Public 
+ * License as published by the Free Software Foundation.
+ * 
+ ************************************************************/
 import java.util.Arrays;
 
 public class SAP {
