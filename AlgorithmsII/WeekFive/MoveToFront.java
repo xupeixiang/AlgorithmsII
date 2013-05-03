55a3f6a9cc362f27f1b5034d3310e2958b856730
Peixiang Xu <peixiangxu@gmail.com>
2013-05-03 20:27:41 +0800

    tab -> space

--- AlgorithmsII/WeekFive/MoveToFront.java
+++ AlgorithmsII/WeekFive/MoveToFront.java
@@ -12,45 +12,45 @@ public class MoveToFront {
     // apply move-to-front encoding, reading from standard input and writing to standard output
     public static void encode(){
     
-    	//sequences intialize
-    	char[] seqs = new char[256];
-    	for(int i = 0;i < 256;i++){
-    		seqs[i] = (char)i;
-    	}
-    	
-    	while(!BinaryStdIn.isEmpty()){
-    		char c = BinaryStdIn.readChar();
-    		char replaceChar = seqs[0];
-    		for(int i = 0;i < seqs.length;i++){
-    			char temp = seqs[i];
-    			if(seqs[i] == c){
-    				BinaryStdOut.write(i, 8);
-    				BinaryStdOut.flush();
-    				seqs[i] = replaceChar;
-    				break;
-    			}else{
-    				seqs[i] = replaceChar;
-    				replaceChar = temp;
-    			}
-    		}
-    		seqs[0] = c;
-    	}
-    	
+        //sequences intialize
+        char[] seqs = new char[256];
+        for(int i = 0;i < 256;i++){
+            seqs[i] = (char)i;
+        }
+        
+        while(!BinaryStdIn.isEmpty()){
+            char c = BinaryStdIn.readChar();
+            char replaceChar = seqs[0];
+            for(int i = 0;i < seqs.length;i++){
+                char temp = seqs[i];
+                if(seqs[i] == c){
+                    BinaryStdOut.write(i, 8);
+                    BinaryStdOut.flush();
+                    seqs[i] = replaceChar;
+                    break;
+                }else{
+                    seqs[i] = replaceChar;
+                    replaceChar = temp;
+                }
+            }
+            seqs[0] = c;
+        }
+        
     }
 
     // apply move-to-front decoding, reading from standard input and writing to standard output
     public static void decode(){
-    	
+        
     }
 
     // if args[0] is '-', apply move-to-front encoding
     // if args[0] is '+', apply move-to-front decoding
     public static void main(String[] args){
-    	if(args[0].equals("-")){
-    		encode();
-    	}
-    	else if(args[0].equals("+")){
-    		decode();
-    	}
+        if(args[0].equals("-")){
+            encode();
+        }
+        else if(args[0].equals("+")){
+            decode();
+        }
     }
 }
