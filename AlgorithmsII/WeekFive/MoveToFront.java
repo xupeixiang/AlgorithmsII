3b81dd3d64445d8651bb2f35e767d55dc53293cf
Peixiang Xu <peixiangxu@gmail.com>
2013-05-03 20:43:54 +0800

    decode function completed

--- AlgorithmsII/WeekFive/MoveToFront.java
+++ AlgorithmsII/WeekFive/MoveToFront.java
@@ -32,15 +32,30 @@ public class MoveToFront {
                     seqs[i] = replaceChar;
                     replaceChar = temp;
                 }
-            }
+            }   
             seqs[0] = c;
-        }
-        
+        }     
     }
 
     // apply move-to-front decoding, reading from standard input and writing to standard output
     public static void decode(){
+        //sequences intialize
+        char[] seqs = new char[256];
+        for(int i = 0;i < 256;i++){
+            seqs[i] = (char)i;
+        }
         
+        while(!BinaryStdIn.isEmpty()){
+            char c = BinaryStdIn.readChar();
+            int index = (int)c;
+            char code = seqs[index];
+            BinaryStdOut.write(code);
+            BinaryStdOut.flush();
+            for(int i = index;i > 0;i--){
+                seqs[i] = seqs[ i - 1];
+            }   
+            seqs[0] = code;
+        }
     }
 
     // if args[0] is '-', apply move-to-front encoding
